9bdb728d98f688b5222a9663fc3da211a58fd79d
TelmanovDmitry <telmanovdmiry@gmail.com>
2022-12-20 12:15:59 +0300

    Create project

---
+++ src/main/java/com/example/vaiechotestbot2/VaiEchoTestBot2Application.java
@@ -0,0 +1,13 @@
+package com.example.vaiechotestbot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaiEchoTestBot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaiEchoTestBot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vaiechotestbot2/VaiEchoTestBot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaiechotestbot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaiEchoTestBot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
